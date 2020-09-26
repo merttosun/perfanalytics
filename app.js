8477a0841783db1ef37020418c153fc2b8578779
merttosun <mert.tosun@windowslive.com>
2020-09-26 22:48:48 +0300

    static serve

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("./perfanalyticjs"));
+app.use(express.static("./perfanalyticsdashboard/build"));
 
 let augmentMiddleware = new AugmentHandlerMiddleware();
 
