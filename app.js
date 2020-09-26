1bf9930f517f71d653bdd0cd7d78d19cc3fe3396
merttosun <mert.tosun@windowslive.com>
2020-09-26 22:17:12 +0300

    dynamic port

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const AugmentHandlerMiddleware = require("./middlewares/augment-handle-middlewar
 const app = express();
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
