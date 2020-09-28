27f9856d23046d0abfd161d86182d44d957a7388
Mert TOSUN <mert.tosun@windowslive.com>
2020-09-28 23:46:08 +0300

    Update app-error.js

--- errors/app-error.js
+++ errors/app-error.js
@@ -6,9 +6,7 @@ class AppError extends Error {
     this.name = this.constructor.name;
     // Capturing stack trace, excluding constructor call from it.
     Error.captureStackTrace(this, this.constructor);
-    // You can use any additional properties you want.
-    // I'm going to use preferred HTTP status for this error types.
-    // `500` is the default value if not specified.
+
     this.status = status || 500;
     this.setCode(code);
     this.setMessage(message);
